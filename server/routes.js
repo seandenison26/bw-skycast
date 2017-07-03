f0c675117e05ccd6de2c1e6dcd4444bfa941a9ec
ziggy <sean.denison26@gmail.com>
2017-07-03 14:03:42 -0500

    added testLocation route for dummy data

--- server/routes.js
+++ server/routes.js
@@ -47,6 +47,15 @@ router.get('/api/forecast/history/:lat/:lng/:time', function(req, res) {
 		.then((data) => res.send(data),console.log);	
 });
 
+//returns a static forecast json file for dev purposes
+router.get('/api/testLocation', function(req, res) {
+	let testLocation = {
+		"formatted_address":"5700 N Natoma Ave, Chicago, IL 60631, USA",
+		"coordinates":{"lat":41.98522699999999,"lng":-87.79414299999999},
+		"place_id":"ChIJc60OqF3JD4gR9LgP8Wnx7VU"	
+	}
+	res.send(testLocation);
+});
 
 //returns a static forecast json file for dev purposes
 router.get('/api/testForecast', function(req, res) {
