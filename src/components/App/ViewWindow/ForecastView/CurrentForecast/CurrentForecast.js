4f987f8bf7430e347ee2c8c6ed9089dec78e3117
ziggy <sean.denison26@gmail.com>
2017-07-09 18:27:50 -0500

    Formatted Current Forecast

--- src/components/App/ViewWindow/ForecastView/CurrentForecast/CurrentForecast.js
+++ src/components/App/ViewWindow/ForecastView/CurrentForecast/CurrentForecast.js
@@ -29,17 +29,22 @@ export default class CurrentForecast extends React.Component {
 
 		return 	<div className="current-forecast">
 				<h2><bold>Current Date </bold>: {DateFormat.getDateString(new Date(this.props.currently.time * 1000))}</h2>
-				<WeatherIcon className="current-icon" icon={`wi-forecast-io-${this.props.currently.icon}`}/>
-				<p>Summary: {this.props.currently.summary}</p>
-				<p>Temperature: {Math.floor(this.props.currently.temperature)}&deg; F</p>	
-				<p>Feels Like: {Math.floor(this.props.currently.apparentTemperature)}&deg; F</p>	
-				<p>Chance of Precipitation: {this.props.currently.precipProbability}&#37;</p>
-				<p>Precipitation: {this.props.currently.precipIntensity} in.</p>
-				<p>Dew Point: {this.props.currently.dewPoint}&deg;</p>
-				<p>Humidity: {this.props.currently.humidity}&#37;</p>
-				<p>Wind Bearing: {this.props.currently.windBearing}</p>
-				<p>Wind Gust: {this.props.currently.windGust}</p>
-				<p>Wind Speed: {this.props.currently.windSpeed}</p>
+				<div className="detail-col">
+					<WeatherIcon className="current-icon" icon={`wi-forecast-io-${this.props.currently.icon}`}/>
+					<p>Summary: {this.props.currently.summary}</p>
+					<p>Temperature: {Math.floor(this.props.currently.temperature)}&deg; F</p>	
+					<p>Feels Like: {Math.floor(this.props.currently.apparentTemperature)}&deg; F</p>	
+					<p>Chance of Precipitation: {this.props.currently.precipProbability}&#37;</p>
+					<p>Precipitation: {this.props.currently.precipIntensity} in.</p>
+					<p>Dew Point: {this.props.currently.dewPoint}&deg; F</p>
+					<p>Humidity: {Math.floor(this.props.currently.humidity * 100)}&#37;</p>
+				</div>
+				
+				<div className="wind-col">
+				<WeatherIcon className="wind-icon" icon={`wi-wind from-${this.props.currently.windBearing}-deg`}/>
+					<p>Wind Gust: {this.props.currently.windGust} mph</p>
+					<p>Wind Speed: {this.props.currently.windSpeed} mph</p>
+				</div>
 			</div>
 	}
 }
--- src/components/App/ViewWindow/WeatherIcon/WeatherIcon.js
+++ src/components/App/ViewWindow/WeatherIcon/WeatherIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './WeatherIcon.css';
 import './css/weather-icons.min.css'
+import './css/weather-icons-wind.min.css'
 import './font/weathericons-regular-webfont.eot'
 import './font/weathericons-regular-webfont.svg'
 import './font/weathericons-regular-webfont.ttf'
