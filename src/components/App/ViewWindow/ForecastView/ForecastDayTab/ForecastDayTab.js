41c8b42967f914ed372f4a125770d0b94f3031f0
ziggy <sean.denison26@gmail.com>
2017-07-09 12:45:37 -0500

    Added background image and applied to index.css

--- src/components/App/ViewWindow/ForecastView/ForecastDayTab/ForecastDayTab.js
+++ src/components/App/ViewWindow/ForecastView/ForecastDayTab/ForecastDayTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ForecastDayTab.css';
+import {Button} from 'react-bootstrap';
 
 export default class ForecastDayTab extends React.Component {
 	constructor(props) {
@@ -13,11 +14,11 @@ export default class ForecastDayTab extends React.Component {
 	}
 
 	render() {
-		return 	<button className="forecast-day-tab col-md-1" onClick={this.handleClick}>
+		return 	<Button className="forecast-day-tab col-md-1" onClick={this.handleClick}>
 				<p>{new Date(this.props.details.time * 1000).getDay()}</p>
 				<p>{this.props.details.icon}</p>
 				<p>{this.props.details.temperatureMax}</p>
 				<p>{this.props.details.temperatureMin}</p>
-			</button>
+			</Button>
 	}
 }
