5bb2d3457fcddd20df324a6684c08263d2716631
ziggy <sean.denison26@gmail.com>
2017-07-09 18:13:43 -0500

    Formatted ForeDetail Text

--- src/components/App/ViewWindow/ForecastView/ForecastDetail/ForecastDetail.js
+++ src/components/App/ViewWindow/ForecastView/ForecastDetail/ForecastDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ForecastDetail.css';
+import WeatherIcon from '../../WeatherIcon/WeatherIcon';
+import {DateFormat} from '../../../../../util.js';
 
 export default class ForecastDetail extends React.Component {
 	constructor(props) {
@@ -10,22 +12,22 @@ export default class ForecastDetail extends React.Component {
 
 	render() {
 		return 	<div className="forecast-detail">
-				<p>Date: {new Date(this.props.details.time * 1000).toString()}</p>
+				<h2>Date: {DateFormat.getDateString(new Date(this.props.details.time * 1000))}</h2>
+				<WeatherIcon className="current-icon" icon={`wi-forecast-io-${this.props.details.icon}`}/>
 				<p>Summary: {this.props.details.summary}</p>
-				<p>ICON: {this.props.details.icon}</p>
-				<p>Sunrise: {new Date(this.props.details.sunriseTime * 1000).toString()}</p>
-				<p>Sunset: {new Date(this.props.details.sunsetTime * 1000).toString()}</p>
+				<p>Sunrise: {DateFormat.getTimeString(new Date(this.props.details.sunriseTime * 1000))}</p>
+				<p>Sunset: {DateFormat.getTimeString(new Date(this.props.details.sunsetTime * 1000))}</p>
 				<p>Moonphase: {(this.props.details.moonPhase)}</p>
-				<p>High: {this.props.details.temperatureMax}</p>			
-				<p>Low: {this.props.details.temperatureMin}</p>			
-				<p>Dew Point: {this.props.details.dewPoint}</p>			
-				<p>Humidity: {this.props.details.humidity}</p>			
+				<p>High: {Math.floor(this.props.details.temperatureMax)}&deg; F</p>			
+				<p>Low: {Math.floor(this.props.details.temperatureMin)}&deg; F</p>			
+				<p>Dew Point: {Math.floor(this.props.details.dewPoint)}&deg; F</p>			
+				<p>Humidity: {this.props.details.humidity * 100}&#37;</p>			
 				<p>Wind Speed: {this.props.details.windSpeed}</p>			
 				<p>Wind Gust: {this.props.details.windGust}</p>			
 				<p>Wind Bearing: {this.props.details.windBearing}</p>			
-				<p>Cloud Cover: {this.props.details.cloudCover}</p>			
-				<p>Pressure: {this.props.details.Pressure}</p>			
-				<p>Ozone: {this.props.details.ozone}</p>			
+				<p>Cloud Cover: {this.props.details.cloudCover * 100}&37;</p>			
+				<p>Pressure: {this.props.details.pressure} millibars</p>			
+				<p>Ozone: {this.props.details.ozone} DU</p>			
 				<p>UV Index: {this.props.details.uvIndex}</p>			
 			</div>
 	}
